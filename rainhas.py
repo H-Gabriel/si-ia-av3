7e3620b32866abd5fbc8d7d4f42c2b397eee2b1d
gabriel rodrigues <gabrielfrhf@gmail.com>
2023-12-08 09:10:50 -0300

    código rainhas finalizado

--- rainhas.py
+++ rainhas.py
@@ -1,14 +1,12 @@
 import numpy as np
 import time
-import sys
 
 n = 100 # Quantidade de indivíduos por geração
-geracao_max = 100_000_000 # Quantidade máxima de gerações
+geracao_max = 10_000 # Quantidade máxima de gerações
 p_recombinacao = 0.9 # Probabilidade de recombinação
 p_mutacao = 0.01 # Probabilidade de mutação
-
-inicio = time.time()
 solucoes = set()
+inicio = time.time()
 
 def calcular_aptidao(individuo):
     pares_atacantes = 0
@@ -18,16 +16,6 @@ def calcular_aptidao(individuo):
         for j in range(vec.shape[0]):
             if vec[j] == j + 1 or vec[j] == 0:
                 pares_atacantes += 1
-    '''
-    if 28 - pares_atacantes == 28 and str(individuo) not in solucoes:
-        fim = time.time()
-        print("ÓTIMO ENCONTRADO", individuo)
-        print("TEMPO:", fim - inicio)
-        solucoes.add(str(individuo))
-    
-        if (len(solucoes) == 92):
-            sys.exit()
-    '''
 
     return 28 - pares_atacantes
 
@@ -47,65 +35,64 @@ def recombinar(pai1, pai2):
     
     return filho1, filho2
 
-def mutar(filho1, filho2):
-    i = np.random.randint(1, 7)
-    j = np.random.randint(1, 7)
-    m_i = np.random.randint(0,8) # Mutação no indice 'i' do filho 1
-    m_j = np.random.randint(0,8) # Mutação no indice 'j' do filho 2
-
-    while m_i == filho1[i]:
-        m_i = np.random.randint(1,7)
-
-    while m_j == filho2[j]:
-        m_j = np.random.randint(1,7)
+def mutar(individuo):
+    for i in range(8):
+        if np.random.random() <= 0.01:
+            mutacao = np.random.randint(0,8)
+            while mutacao == individuo[i]:
+                mutacao = np.random.randint(0,8)
+            individuo[i] = mutacao
     
-    filho1[i] = m_i
-    filho2[j] = m_j
+    return individuo
 
-    return filho1, filho2
+def run():
+    # Definindo população inicial
+    P = np.random.randint(low=0, high=8, size=(n,8))
 
-# Definindo população inicial
-P = np.random.randint(low=0, high=8, size=(n,8))
+    geracao_atual = 0
+    while geracao_atual < geracao_max:
+        # Calcula a aptidão total da geração e salva a aptidão de cada individuo
+        aptidoes = [None] * n
+        for i in range(n):
+            aptidoes[i] = calcular_aptidao(P[i,:])
+            if aptidoes[i] == 28:
+                if str(P[i]) not in solucoes:
+                    print("ÓTIMO ENCONTRADO", P[i], "GERAÇÃO ATUAL:", geracao_atual)
+                    solucoes.add(str(P[i]))
+                return
+        
+        aptidao_total = sum(aptidoes)
+        geracao_atual += 1
 
-geracao_atual = 0
-while geracao_atual < geracao_max:
-    # Calcula a aptidão total da geração e salva a aptidão de cada 
-    aptidoes = [None] * n
-    for i in range(n):
-        aptidoes[i] = calcular_aptidao(P[i,:])
-    aptidao_total = sum(aptidoes)
-    geracao_atual += 1
+        # Conjunto de seleção de acordo com o método da roleta
+        S = np.empty((n,8), dtype=int)
+        for i in range(n):
+            j = 0
+            soma = aptidoes[j]/aptidao_total
+            r = np.random.uniform()
+            while soma < r:
+                j += 1
+                soma += aptidoes[j] /aptidao_total
 
-    if geracao_atual % 500 == 0:
-        fim = time.time()
-        print(fim - inicio)
-        print("Geração:", geracao_atual)
-        inicio = time.time()
+            S[i] = P[j]
 
-    # Conjunto de seleção de acordo com o método da roleta
-    S = np.empty((n,8), dtype=int)
-    for i in range(n):
-        j = 0
-        soma = aptidoes[j]/aptidao_total
-        r = np.random.uniform()
-        while soma < r:
-            j += 1
-            soma += aptidoes[j] /aptidao_total
+        # Recombinação dos individuos selecionados
+        for i in range(0, n, 2):
+            if np.random.random() <= p_recombinacao: # Apto a recombinar
+                filho1, filho2 = recombinar(S[i, :], S[i+1, :])
+                #if np.random.random() <= 0.01: # Chance de mutação atendida
+                filho1 = mutar(filho1)
+                filho2 = mutar(filho2)
 
-        S[i] = P[j]
+                P[i] = filho1
+                P[i+1] = filho2
+                continue
 
-    # Recombinação dos individuos selecionados
-    for i in range(0, n, 2):
-        if np.random.random() <= p_recombinacao: # Apto a recombinar
-            filho1, filho2 = recombinar(S[i, :], S[i+1, :])
-            if np.random.random() <= 0.01: # Chance de mutação atendida
-                filho1, filho2 = mutar(filho1, filho2)
+            P[i] = S[i, :]
+            P[i+1] = S[i+1, :]
 
-            P[i] = filho1
-            P[i+1] = filho2
-            continue
+while (len(solucoes) < 92):
+    run()
 
-        P[i] = S[i, :]
-        P[i+1] = S[i+1, :]
 fim = time.time()
-print(fim - inicio)
+print("TEMPO GASTO AO TOTAL:", fim - inicio)
