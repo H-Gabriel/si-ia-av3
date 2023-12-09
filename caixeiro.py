400f7b569d20f14d6854f425ca4517c9700973c1
gabriel rodrigues <gabrielfrhf@gmail.com>
2023-12-09 15:51:28 -0300

    progresso no caixeiro viajante

--- caixeiro.py
+++ caixeiro.py
@@ -37,12 +37,13 @@ def calcular_inaptidao(individuo, pontos, p_origem):
     return inaptidao
 
 def recombinar(pai1, pai2):
-    p1 = np.random.randint(1, pai1.shape[1] - 1)
-    p2 = np.random.randint(1, pai2.shape[1] - 1)
+    k = pai1.shape[1] # Quantidade de cromossomos
+    p1 = np.random.randint(1, k - 1)
+    p2 = np.random.randint(1, k - 1)
 
     while p1 == p2:
-        p1 = np.random.randint(1, pai1.shape[1] - 1)
-        p2 = np.random.randint(1, pai2.shape[1] - 1)
+        p1 = np.random.randint(1, k - 1)
+        p2 = np.random.randint(1, k - 1)
     
     if p1 > p2:
         p1, p2 = p2, p1
@@ -50,74 +51,130 @@ def recombinar(pai1, pai2):
     filho1 = pai1[0][p1:p2+1]
     filho2 = pai2[0][p1:p2+1]
 
-    r1 = np.concatenate((pai1[0][p2+1:], pai1[0][:p1]))
-    r2 = np.concatenate((pai2[0][p2+1:], pai2[0][:p1]))
+    restante1 = np.concatenate((pai1[0][p2+1:], pai1[0][:p2+1]))
+    restante2 = np.concatenate((pai2[0][p2+1:], pai2[0][:p2+1]))
 
-    for i in range(r1.shape[0]):
-        temp = []
-        while len(temp) != p1:
-            if (r1[i] not in filho1):
-                temp.append(r1[i])
-        
+    temp = [x for x in restante2 if x not in filho1]
+    i = p2 + 1
+    while(i < k):
+        filho1 = np.concatenate((filho1, np.array([temp.pop(0)])))
+        i += 1
+    temp = [x for x in restante2 if x not in filho1]
+    filho1 = np.concatenate((temp, filho1))
+
+    temp = [x for x in restante1 if x not in filho2]
+    i = p2 + 1
+    while(i < k):
+        filho2 = np.concatenate((filho2, np.array([temp.pop(0)])))
+        i += 1
+    temp = [x for x in restante1 if x not in filho2]
+    filho2 = np.concatenate((temp, filho2))
 
     return filho1, filho2
 
 def mutar(individuo):
-    for i in range(individuo.shape[0]):
+    for i in range(individuo.shape[0] - 1):
         if np.random.random() <= 0.01:
             j = np.random.randint(i+1, individuo.shape[0])
             individuo[i], individuo[j] = individuo[j], individuo[i]
+    if np.random.random() <= 0.01:
+        i = np.random.randint(0, individuo.shape[0] - 1)
+        individuo[individuo.shape[0] - 1], individuo[i] = individuo[i], individuo[individuo.shape[0] - 1]
+    return individuo
 
-def run():
-    n = 50 # Quantidade de individuos por geração
-    geracao_max = 10_000 # Quantidade máxima de gerações
-    n_pontos = 31 # Pontos por região
-    pontos = gerar_pontos(n_pontos) # Gera os pontos
-    i_origem = np.random.randint(0, pontos.shape[0]) # Escolhe um indice para o ponto de origem
-    p_origem = pontos[i_origem,:].reshape(1,3) # Guarda a origem
-    pontos = np.delete(pontos, i_origem, axis=0) # Deleta a origem da "pool" de pontos a serem acessados
+n = 50 # Quantidade de individuos por geração
+geracao_max = 10000 # Quantidade máxima de gerações
+p_recombinacao = 0.9
+n_pontos = 30 # Pontos por região
+pontos = gerar_pontos(n_pontos) # Gera os pontos
+i_origem = np.random.randint(0, pontos.shape[0]) # Escolhe um indice para o ponto de origem
+p_origem = pontos[i_origem,:].reshape(1,3) # Guarda a origem
+pontos = np.delete(pontos, i_origem, axis=0) # Deleta a origem da "pool" de pontos a serem acessados
+melhor_inaptidao = float('inf')
 
-    # Definindo população inicial
-    P = np.empty((n, pontos.shape[0]), dtype=int)
+melhor_caminho = None
+melhoria_enontrada = False
+geracoes_sem_melhoria = 0
+
+# Definindo população inicial
+P = np.empty((n, pontos.shape[0]), dtype=int)
+for i in range(n):
+    individuo = np.random.permutation(pontos.shape[0]).reshape(1,pontos.shape[0])
+    P[i] = individuo
+
+geracao_atual = 0
+while geracao_atual < geracao_max:
+    # Calcula a aptidão total da geração e salva a aptidão de cada individuo
+    inaptidoes = [None] * n
     for i in range(n):
-        individuo = np.random.permutation(pontos.shape[0]).reshape(1,pontos.shape[0])
-        P[i] = individuo
+        inaptidoes[i] = calcular_inaptidao(P[i,:], pontos, p_origem)
+        if (inaptidoes[i] < melhor_inaptidao):
+            melhor_inaptidao = inaptidoes[i]
+            melhor_caminho = P[i,:]
+            melhoria_enontrada = True
+            geracoes_sem_melhoria = 0
+            print("MELHORIA ENCONTRADA. GERAÇÃO:", geracao_atual, "APTIDÃO:", melhor_inaptidao)
 
-    geracao_atual = 0
-    while geracao_atual < geracao_max:
-        # Calcula a aptidão total da geração e salva a aptidão de cada individuo
-        inaptidoes = [None] * n
-        for i in range(n):
-            inaptidoes[i] = calcular_inaptidao(P[i,:], pontos, p_origem)
-        geracao_atual += 1
+    if melhoria_enontrada == False:
+        geracoes_sem_melhoria += 1
 
-        # Conjunto de seleção de acordo com o método do torneio
-        S = np.empty((n, pontos.shape[0]), dtype=int)
-        for i in range(n):
-            torneio = np.random.randint(0, n, size=2) # Índice dos individuos selecionados
-            if inaptidoes[torneio[0]] < inaptidoes[torneio[1]]:
-                S[i] = P[torneio[0]]
-                continue
-            S[i] = P[torneio[1]]
-        
-        # Recombinação dos individuos
-        for i in range(0, n, 2):
-            filho1, filho2 = recombinar(S[i, :].reshape(1, pontos.shape[0]), S[i+1, :].reshape(1, pontos.shape[0]))
+    if (geracoes_sem_melhoria == 1000):
+        break
+    
+    melhoria_encontrada = False
+
+    geracao_atual += 1
+
+    if (geracao_atual % 100 == 0):
+        print("GERAÇÃO ATUAL:", geracao_atual)
+
+    # Preservando os individuos de elite para a próxima geração
+    indices_melhores = np.argsort(inaptidoes)
+    P[0] = P[indices_melhores[0]]
+    P[1] = P[indices_melhores[1]]
+
+    # Conjunto de seleção de acordo com o método do torneio
+    S = np.empty((n-2, pontos.shape[0]), dtype=int) # n-2 pois existem 2 individuos de elite preservados
+    for i in range(n-2):
+        torneio = np.random.randint(0, n, size=2) # Índice dos individuos selecionados
+        if inaptidoes[torneio[0]] < inaptidoes[torneio[1]]:
+            S[i] = P[torneio[0]]
+            continue
+        S[i] = P[torneio[1]]
+
+    # Recombinação dos individuos
+    for i in range(0, n-2, 2): # n-2 pois existem 2 individuos de elite preservados
+        if np.random.random() <= p_recombinacao: # Apto a recombinar
+            filho1, filho2 = recombinar(S[i].reshape(1, pontos.shape[0]), S[i+1].reshape(1, pontos.shape[0]))
             filho1 = mutar(filho1)
             filho2 = mutar(filho2)
+            P[i+2] = filho1
+            P[i+3] = filho2
+            continue
 
+        P[i+2] = S[i,:]
+        P[i+3] = S[i+1, :]
+    
 
-run()
-'''
 fig = plt.figure()
 ax = fig.add_subplot(projection='3d')
 ax.scatter(pontos[:,0], pontos[:,1], pontos[:,2], c='#248DD2', marker='o')
 ax.scatter(p_origem[0:,0], p_origem[0:,1], p_origem[0:,2], c='green', marker='x',linewidth=3,s=30)
+plt.pause(2)
 
-#exemplo caminho a partir da origem.
-p2 = pontos[0,:].reshape(1,3)
+p2 = pontos[melhor_caminho[0]].reshape(1,3)
 line, = ax.plot([p_origem[0,0],p2[0,0]],[p_origem[0,1],p2[0,1]],[p_origem[0,2],p2[0,2]],color='k')
+plt.pause(2)
 
-plt.tight_layout()
+for i in range(pontos.shape[0] - 1):
+    p1 = pontos[melhor_caminho[i]]
+    p2 = pontos[melhor_caminho[i+1]]
+    line, = ax.plot([p1[0],p2[0]], [p1[1],p2[1]], [p1[2],p2[2]], color='k')
+    plt.pause(2)
+
+p1 = pontos[melhor_caminho[pontos.shape[0] - 1]]
+line, = ax.plot([p1[0],p_origem[0,0]], [p1[1],p_origem[0,1]], [p1[2],p_origem[0,2]], color='k')
+plt.pause(60)
+
+#plt.tight_layout()
 plt.show()
-'''
